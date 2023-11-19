e348c9c3b279188fc4989258cea1fe8b0997f25e
Abdul <lhlrahman@gmail.com>
2023-11-18 20:30:02 -0500

    fix media query

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -22,6 +22,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
             <div className="mt-16"></div>
+            <div className='sm: mt-16 md:mt-10'></div>
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
               <p className='py-4'>These are most of the technologies I've worked with</p>
           </div>
