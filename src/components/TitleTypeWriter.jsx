a5326b44ba26461ea4f1baf9362d6f486b9809de
Abdul <lhlrahman@gmail.com>
2023-11-26 23:28:14 -0500

    change emoji to uni code to work for mobile

--- src/components/TitleTypeWriter.jsx
+++ src/components/TitleTypeWriter.jsx
@@ -1,31 +1,30 @@
 "use client"
-import React from 'react'
-import TypeWriter from 'typewriter-effect'
+import React from 'react';
+import TypeWriter from 'typewriter-effect';
 
 const TitleTypeWriter = () => {
   return (
     <TypeWriter
-    options={{
-      loop: true,
-    }}
-    onInit={(typewriter) => {
-      typewriter.typeString('a Software Engineer 💻')
-        .pauseFor(500)
-        .deleteAll()
-        .typeString('A Full Stack Developer 🖥️')
-        .pauseFor(500)
-        .deleteAll()
-        .typeString('an Air Cadet 👨‍✈️')
-        .pauseFor(500)
-        .deleteAll()
-        .typeString('Batman 🦇')
-        .pauseFor(500)
-        .deleteAll()
-        .start();
-    }}
-    
+      options={{
+        loop: true,
+      }}
+      onInit={(typewriter) => {
+        typewriter.typeString('a Software Engineer \u{1F4BB}') // Unicode for 💻
+          .pauseFor(500)
+          .deleteAll()
+          .typeString('A Full Stack Developer \u{1F5A5}') // Unicode for 🖥️
+          .pauseFor(500)
+          .deleteAll()
+          .typeString('an Air Cadet \u{1F468}\u200D\u2708\uFE0F') // Unicode for 👨‍✈️
+          .pauseFor(500)
+          .deleteAll()
+          .typeString('Batman \u{1F987}') // Unicode for 🦇
+          .pauseFor(500)
+          .deleteAll()
+          .start();
+      }}
     />
   )
 }
 
-export default TitleTypeWriter
+export default TitleTypeWriter;
