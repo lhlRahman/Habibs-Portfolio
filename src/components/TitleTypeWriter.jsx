573247a4e30e57559894f3dfabfc4fe24f41bb03
Abdul <lhlrahman@gmail.com>
2023-11-28 23:56:50 -0500

    Fixed bug of emoji not showing on moblie

--- src/components/TitleTypeWriter.jsx
+++ src/components/TitleTypeWriter.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import TypeWriter from 'typewriter-effect';
+import aircadet from '../assets/aircadet.png';
+import bat from '../assets/bat.png';
+import desktop from '../assets/desktop.png';
+import laptop from '../assets/laptop.png';
 
 const TitleTypeWriter = () => {
-  const emojiFontClass = 'font-system-ui'; // This is the class that applie
-  
   return (
     <TypeWriter
       options={{
         loop: true,
       }}
       onInit={(typewriter) => {
-        typewriter.typeString('a Software Engineer <span class="' + emojiFontClass + '">&#x1F4BB;</span>')
+        typewriter
+          .typeString(`a Software Engineer <img src="${desktop}" alt="desktop" class="inline h-8" />`)
           .pauseFor(500)
           .deleteAll()
-          .typeString('A Full Stack Developer <span class="' + emojiFontClass + '">&#x1F4BB;</span>')
+          .typeString(`A Full Stack Developer <img src="${laptop}" alt="laptop" class="inline h-8" />`)
           .pauseFor(500)
           .deleteAll()
-          .typeString('an Air Cadet <span class="' + emojiFontClass + '">&#x1F6E2;&#x200D;&#x2708;&#xFE0F;</span>')
+          .typeString(`an Air Cadet <img src="${aircadet}" alt="aircadet" class="inline h-8" />`)
           .pauseFor(500)
           .deleteAll()
-          .typeString('Batman <span class="' + emojiFontClass + '">&#x1F987;</span>')
+          .typeString(`Batman <img src="${bat}" alt="bat" class="inline h-8" />`)
           .pauseFor(500)
           .deleteAll()
           .start();
       }}
     />
-  )
-}
+  );
+};
 
 export default TitleTypeWriter;
