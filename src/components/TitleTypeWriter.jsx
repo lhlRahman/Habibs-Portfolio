cb9e19687d59a9feb9b266735c055905b902148f
Abdul <lhlrahman@gmail.com>
2023-11-28 20:33:22 -0500

    fix emoji

--- src/components/TitleTypeWriter.jsx
+++ src/components/TitleTypeWriter.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from 'react';
 import TypeWriter from 'typewriter-effect';
 
@@ -9,16 +9,16 @@ const TitleTypeWriter = () => {
         loop: true,
       }}
       onInit={(typewriter) => {
-        typewriter.typeString('a Software Engineer \u{1F4BB}') // Unicode for 💻
+        typewriter.typeString('a Software Engineer 💻')
           .pauseFor(500)
           .deleteAll()
-          .typeString('A Full Stack Developer \u{1F5A5}') // Unicode for 🖥️
+          .typeString('A Full Stack Developer 🖥️')
           .pauseFor(500)
           .deleteAll()
-          .typeString('an Air Cadet \u{1F468}\u200D\u2708\uFE0F') // Unicode for 👨‍✈️
+          .typeString('an Air Cadet 👨‍✈️')
           .pauseFor(500)
           .deleteAll()
-          .typeString('Batman \u{1F987}') // Unicode for 🦇
+          .typeString('Batman 🦇')
           .pauseFor(500)
           .deleteAll()
           .start();
