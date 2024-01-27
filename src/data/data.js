1ad63482ac4bc769518f0ecf6d00090b8c5d92a8
Abdul <lhlrahman@gmail.com>
2024-01-26 21:22:26 -0500

    Add New Project

--- src/data/data.js
+++ src/data/data.js
@@ -2,31 +2,39 @@ import Notetionai from '../assets/notetion.png'
 import LocalMarket from '../assets/localmarket.png'
 import Habibsblog from '../assets/habibblog.png'
 import Photolabs from '../assets/photolabs.png'
+import Odyssey from '../assets/odysseylogo.png'
 
 export const data=[
     {
         id:1,
+        name:"Odyssey",
+        image: Odyssey,
+        github:'https://github.com/ubctliu/odyssey/',
+        live:"https://odyssey-delta-lac.vercel.app",
+    },
+    {
+        id:2,
         name:"Note-Tion AI",
         image:Notetionai,
         github:"https://github.com/lhlRahman/Note-TionAI",
         live:"https://note-tion-ai.vercel.app/",
     },
     {
-        id:2,
+        id:3,
         name:"Local Market",
         image:LocalMarket,
         github:"https://github.com/lhlRahman/LocalMarket",
         live:"",
     },
     {
-        id:3,
+        id:4,
         name:"Habib's Blog",
         image: Habibsblog,
         github:"https://github.com/lhlRahman/MyBlog",
         live:"https://whoishabib.wiki/",
     },
     {
-        id:4,
+        id:5,
         name:"Photolabs",
         image:Photolabs,
         github:"https://github.com/lhlRahman/photolabs-starter",
